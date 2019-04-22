31971894cccf2327b4c6823fe64b1ecbc69d6786
Rafael Makaha <rafael.makaha@gmail.com>
2019-04-22 10:57:27 -0300

    Adds primary Key on answares Model.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- answares/models.py
+++ answares/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Answares(models.Model):
+    lime_id = models.IntegerField(
+        primary_key=True
+    )
     servico_id = models.IntegerField()
     servico_nome = models.CharField(
         max_length=120
